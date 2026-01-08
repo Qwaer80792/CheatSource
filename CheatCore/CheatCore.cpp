2446ed7c72d25430af80b1f3465751772b58c59d
QwaYer <mazurkevicdenis05@gamil.com>
2026-01-08 15:02:14 +0300

    Обход двойного рендеринга(фикс структуры imgui)

--- CheatCore/CheatCore.cpp
+++ CheatCore/CheatCore.cpp
@@ -1,51 +1,132 @@
-﻿#include <windows.h> 
+﻿#include <windows.h>
 #include <d3d9.h>
 #include <d3dx9.h>
-
+#include <iostream>
 #include "imgui.h"
 #include "imgui_impl_dx9.h"
 #include "imgui_impl_win32.h"
 #include "MinHook.h"
 
+#pragma comment(lib, "d3d9.lib")
+#pragma comment(lib, "d3dx9.lib")
+#pragma comment(lib, "MinHook.x86.lib")
+
+// --- Типы функций ---
 typedef HRESULT(STDMETHODCALLTYPE* EndScene_t)(IDirect3DDevice9*);
+typedef HRESULT(STDMETHODCALLTYPE* Reset_t)(IDirect3DDevice9*, D3DPRESENT_PARAMETERS*);
+typedef BOOL(WINAPI* SetCursorPos_t)(int, int);
+
+// --- Глобальные переменные ---
 EndScene_t Original_EndScene = nullptr;
+Reset_t Original_Reset = nullptr;
+WNDPROC Original_WndProc = nullptr;
+SetCursorPos_t Original_SetCursorPos = nullptr;
 
 bool init = false;
 bool show_menu = true;
+HWND game_hwnd = nullptr;
+
+// Для фильтрации CEF (фантомных окон)
+LONGLONG last_pc = 0;
+LONGLONG frequency = 0;
 
 extern IMGUI_IMPL_API LRESULT ImGui_ImplWin32_WndProcHandler(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam);
-WNDPROC Original_WndProc = nullptr;
 
+// --- Хук на перемещение курсора игрой ---
+BOOL WINAPI Hooked_SetCursorPos(int X, int Y) {
+    // Если меню открыто, блокируем попытки игры центрировать мышь
+    if (show_menu) return TRUE;
+    return Original_SetCursorPos(X, Y);
+}
+
+// --- Обработка ввода (WndProc) ---
 LRESULT CALLBACK Hooked_WndProc(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam) {
-    if (show_menu && ImGui_ImplWin32_WndProcHandler(hWnd, msg, wParam, lParam))
-        return true;
+    if (show_menu) {
+        ImGui_ImplWin32_WndProcHandler(hWnd, msg, wParam, lParam);
+
+        // Блокируем ввод в игру, если мышь над меню
+        ImGuiIO& io = ImGui::GetIO();
+        if (io.WantCaptureMouse) return true;
+    }
     return CallWindowProc(Original_WndProc, hWnd, msg, wParam, lParam);
 }
 
+// --- Хук Reset (для корректного сворачивания игры) ---
+HRESULT STDMETHODCALLTYPE Hooked_Reset(IDirect3DDevice9* pDevice, D3DPRESENT_PARAMETERS* pPresentationParameters) {
+    if (init) ImGui_ImplDX9_InvalidateDeviceObjects();
+    HRESULT hr = Original_Reset(pDevice, pPresentationParameters);
+    if (SUCCEEDED(hr) && init) ImGui_ImplDX9_CreateDeviceObjects();
+    return hr;
+}
+
+// --- Основной хук отрисовки ---
 HRESULT STDMETHODCALLTYPE Hooked_EndScene(IDirect3DDevice9* pDevice) {
-    if (!init) {
-        ImGui::CreateContext();
-        ImGuiIO& io = ImGui::GetIO();
+    // 1. Переключение меню (INSERT)
+    if (GetAsyncKeyState(VK_INSERT) & 1) {
+        show_menu = !show_menu;
 
-        D3DDEVICE_CREATION_PARAMETERS cp;
-        pDevice->GetCreationParameters(&cp);
-
-        Original_WndProc = (WNDPROC)SetWindowLongPtr(cp.hFocusWindow, GWLP_WNDPROC, (LONG_PTR)Hooked_WndProc);
-
-        ImGui_ImplWin32_Init(cp.hFocusWindow);
-        ImGui_ImplDX9_Init(pDevice);
-        init = true;
+        // Показываем/скрываем системный курсор
+        if (show_menu) {
+            ClipCursor(NULL);
+            while (ShowCursor(TRUE) < 0);
+        }
+        else {
+            while (ShowCursor(FALSE) >= 0);
+        }
     }
 
-    if (show_menu) {
+    // 2. Фильтрация CEF (чтобы не мигало и не двоилось)
+    LARGE_INTEGER current_pc;
+    QueryPerformanceCounter(&current_pc);
+    if (frequency == 0) QueryPerformanceFrequency((LARGE_INTEGER*)&frequency);
+    double interval = (double)(current_pc.QuadPart - last_pc) / frequency;
+    if (interval < 0.002) return Original_EndScene(pDevice);
+    last_pc = current_pc.QuadPart;
+
+    // 3. Инициализация ImGui
+    if (!init) {
+        D3DDEVICE_CREATION_PARAMETERS cp;
+        if (SUCCEEDED(pDevice->GetCreationParameters(&cp))) {
+            game_hwnd = cp.hFocusWindow;
+            if (game_hwnd) {
+                ImGui::CreateContext();
+                ImGui_ImplWin32_Init(game_hwnd);
+                ImGui_ImplDX9_Init(pDevice);
+
+                ImGuiIO& io = ImGui::GetIO();
+                io.IniFilename = nullptr;
+                io.MouseDrawCursor = true; // ImGui сам рисует курсор
+
+                Original_WndProc = (WNDPROC)SetWindowLongPtr(game_hwnd, GWLP_WNDPROC, (LONG_PTR)Hooked_WndProc);
+                init = true;
+            }
+        }
+    }
+
+    // 4. Отрисовка меню
+    if (init && show_menu) {
         ImGui_ImplDX9_NewFrame();
         ImGui_ImplWin32_NewFrame();
         ImGui::NewFrame();
 
-        ImGui::Begin("Cheat Project v1.0", &show_menu);
-        ImGui::Text("Welcome to the Internal Menu");
-        if (ImGui::Button("Unload DLL")) {
+        ImGui::SetNextWindowSize(ImVec2(400, 300), ImGuiCond_FirstUseEver);
+        ImGui::Begin("Radmir Internal Final v3.5", &show_menu);
+
+        ImGui::Text("Status: Fully Unlocked");
+        ImGui::Separator();
+
+        if (ImGui::CollapsingHeader("Player Hacks")) {
+            static bool godmode = false;
+            ImGui::Checkbox("GodMode (Visual)", &godmode);
+
+            static float speed = 1.0f;
+            ImGui::SliderFloat("Move Speed", &speed, 1.0f, 10.0f);
         }
+
+        if (ImGui::Button("Unload Cheat")) {
+            // Логика выгрузки (по желанию)
+        }
+
         ImGui::End();
 
         ImGui::EndFrame();
@@ -56,38 +137,38 @@ HRESULT STDMETHODCALLTYPE Hooked_EndScene(IDirect3DDevice9* pDevice) {
     return Original_EndScene(pDevice);
 }
 
-DWORD WINAPI MainThread(LPVOID lpReserved) {
-    while (!GetModuleHandleA("d3d9.dll")) Sleep(100);
-
+// --- Поиск VTable ---
+void* GetVTableFunction(int index) {
     IDirect3D9* pD3D = Direct3DCreate9(D3D_SDK_VERSION);
-    if (!pD3D) return FALSE;
-
     D3DPRESENT_PARAMETERS d3dpp = { 0 };
     d3dpp.Windowed = TRUE;
     d3dpp.SwapEffect = D3DSWAPEFFECT_DISCARD;
-    d3dpp.hDeviceWindow = GetForegroundWindow();
-
+    d3dpp.hDeviceWindow = GetDesktopWindow();
     IDirect3DDevice9* pDummyDevice = nullptr;
-    if (pD3D->CreateDevice(D3DADAPTER_DEFAULT, D3DDEVTYPE_HAL, d3dpp.hDeviceWindow, D3DCREATE_SOFTWARE_VERTEXPROCESSING, &d3dpp, &pDummyDevice) != D3D_OK) {
+    if (pD3D->CreateDevice(D3DADAPTER_DEFAULT, D3DDEVTYPE_HAL, d3dpp.hDeviceWindow, D3DCREATE_SOFTWARE_VERTEXPROCESSING, &d3dpp, &pDummyDevice) == S_OK) {
+        void** vTable = *(void***)pDummyDevice;
+        void* func = vTable[index];
+        pDummyDevice->Release();
         pD3D->Release();
-        return FALSE;
+        return func;
     }
+    return nullptr;
+}
 
-    void** vTable = *(void***)pDummyDevice;
+// --- Поток чита ---
+DWORD WINAPI MainThread(LPVOID lpReserved) {
+    while (!GetModuleHandleA("d3d9.dll")) Sleep(500);
 
     MH_Initialize();
-    MH_CreateHook(vTable[42], &Hooked_EndScene, (LPVOID*)&Original_EndScene);
-    MH_EnableHook(vTable[42]);
 
-    pDummyDevice->Release();
-    pD3D->Release();
+    // Хукаем DirectX
+    MH_CreateHook(GetVTableFunction(42), &Hooked_EndScene, (LPVOID*)&Original_EndScene);
+    MH_CreateHook(GetVTableFunction(16), &Hooked_Reset, (LPVOID*)&Original_Reset);
 
-    while (true) {
-        if (GetAsyncKeyState(VK_INSERT) & 1) {
-            show_menu = !show_menu;
-        }
-        Sleep(10);
-    }
+    // Хукаем курсор (чтобы не центрировался)
+    MH_CreateHookApi(L"user32.dll", "SetCursorPos", &Hooked_SetCursorPos, (LPVOID*)&Original_SetCursorPos);
+
+    MH_EnableHook(MH_ALL_HOOKS);
 
     return TRUE;
 }
@@ -95,7 +176,7 @@ DWORD WINAPI MainThread(LPVOID lpReserved) {
 BOOL APIENTRY DllMain(HMODULE hModule, DWORD reason, LPVOID lpReserved) {
     if (reason == DLL_PROCESS_ATTACH) {
         DisableThreadLibraryCalls(hModule);
-        CreateThread(nullptr, 0, MainThread, hModule, 0, nullptr);
+        CreateThread(nullptr, 0, (LPTHREAD_START_ROUTINE)MainThread, hModule, 0, nullptr);
     }
     return TRUE;
 }
